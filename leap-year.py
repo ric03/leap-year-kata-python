e0e0dda20079a9efca514e03d217c0c233c92a1a
Tom Schneider <ric0.github@gmail.com>
2022-08-06 01:20:14 +0200

    refactor: merge the two True conditions into one

--- leap-year.py
+++ leap-year.py
@@ -9,9 +9,8 @@ def is_leap_year(year):
     :param year: number
     :return: boolean
     """
-    if year % 400 == 0:
-        return True
-    elif year % 4 == 0 and year % 100 != 0:
+    if (year % 400 == 0) or \
+            (year % 4 == 0 and year % 100 != 0):
         return True
     else:
         return False
